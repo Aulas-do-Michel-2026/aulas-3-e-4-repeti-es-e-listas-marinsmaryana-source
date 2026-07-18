90f4141512e17a4e3c17015bd5cf96f0de7f3f6f
marinsmaryana-source <marins.maryana@gmail.com>
2026-07-18 11:06:29 -0300

    Update exercicio_3.py

--- exercicio_3.py
+++ exercicio_3.py
@@ -22,9 +22,6 @@ Os números ímpares são [1, 3, 5]
 # Código para pegar a lista
 lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
 
-# Código para pegar a lista
-lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
-
 # Lista que armazenará os ímpares
 impares = []
 
